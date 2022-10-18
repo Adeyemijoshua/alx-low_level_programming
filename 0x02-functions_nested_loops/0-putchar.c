b4b24b99122594deb76a9e5766911f3f2052b8b3
adeyemi joshua <adeyemijoshua9999@gmail.com>
2022-10-18 14:06:59 -0700

    e a program that prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,10 +10,10 @@ int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int count, siz;
 
 	siz = sizeof(str) / sizeof(int);
-	for (count = 0; count < siz; count++)	
+	for (count = 0; count < siz; count++)
 {
 		_putchar(str[count]);
-}	
+}
 		_putchar('\n');
 		return (0);
 }

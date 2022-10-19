b250bd681f9f667dbe3580aa0d0e9cb0731de038
adeyemi joshua <adeyemijoshua9999@gmail.com>
2022-10-18 22:22:47 -0700

    function that prints all natural numbers from n to 98,

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,23 +12,23 @@ void print_to_98(int n)
 {
 if (n <= 98)
 {
-for (; n <= 98; n++)
+	for (; n <= 98; n++)
 {
-printf("%d", n);
-if (n == 98)
-	continue;
-printf(", ");
+		printf("%d", n);
+		if (n == 98)
+			continue;
+		printf(", ");
 }
 else
 {
-for (; n >= 98; n--)
+	for (; n >= 98; n--)
 {
-printf("%d", n);
-if (n == 98)
-	continue;
-printf(", ");
+		printf("%d", n);
+		if (n == 98)
+			continue;
+		printf(", ");
 }
-printf("\n");
+		printf("\n");
 }
 }
 

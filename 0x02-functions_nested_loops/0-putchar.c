933215af8c394cdc0e7e793c2b710097f7fa1383
adeyemi joshua <adeyemijoshua9999@gmail.com>
2022-10-18 20:46:49 -0700

    updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,9 +10,9 @@ int main(void)
 		char ch[8] = "_putchar";
 		int i;
 
-		for (i = 0; i < 8; i++)
-				_putchar(ch[i]);
-			_putchar('\n');
+for (i = 0; i < 8; i++)
+	_putchar(ch[i]);
+	_putchar('\n');
 
 			return (0);
 }

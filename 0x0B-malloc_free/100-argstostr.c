fce3853bf5b03e4e974216d5fce39b6867e54a0d
adeyemi joshua <adeyemijoshua9999@gmail.com>
2022-11-09 15:22:55 -0800

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,10 +1,13 @@
 #include "main.h"
- /**
-  * argstostr - prints args
-  * @ac: takes in width of grid
-  * @av: height of grid
-  * Return: the args one line at a time
-  */
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of a program.
+ * @ac: argument count.
+ * @av: argument vector.
+ *
+ * Return: pointer of an array of char
+ */
 
 char *argstostr(int ac, char **av)
 {
